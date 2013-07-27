f8961040ce6a2ef13c29ab7016dc18698f64c1eb
Willi Schönborn <w.schoenborn@gmail.com>
2013-07-27 18:16:54 +0200

    Added bigger integration test

--- src/test/java/org/whiskeysierra/process/PrimalIntegrationTest.java
+++ src/test/java/org/whiskeysierra/process/PrimalIntegrationTest.java
@@ -1,18 +1,27 @@
 package org.whiskeysierra.process;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static java.nio.file.Paths.get;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assume.assumeThat;
 import static org.whiskeysierra.process.PathMatchers.exists;
 
+// TODO run integration tests separately from unit tests
 public final class PrimalIntegrationTest {
+
+    @Rule
+    public final TemporaryFolder temp = new TemporaryFolder();
 
     @Test
     public void readCommand() throws IOException {
@@ -29,6 +38,28 @@ public final class PrimalIntegrationTest {
 
         final String output = Primal.read(python, "-c", "print 'Hello World'");
         assertThat(output.trim(), equalTo("Hello World"));
+    }
+
+    @Test
+    public void redirectOutputToFile() throws IOException {
+        // TODO introduce anntotation and rule for that
+        assumeThat(Os.getCurrent().getFamilies(), hasItem(Family.UNIX));
+
+        final ProcessService service = Primal.createService();
+
+        final Path input = Paths.get("src/test/resources/pg100.txt");
+        final Path output = temp.newFile().toPath();
+
+        final ManagedProcess managed = service.prepare("cat", input.toAbsolutePath().toString());
+
+        managed.redirect(Stream.OUTPUT, Redirection.to(output));
+        managed.redirect(Stream.ERROR, Redirection.NULL);
+
+        managed.call().await();
+
+        final byte[] written = Files.readAllBytes(output);
+
+        assertThat(written, equalTo(Files.readAllBytes(input)));
     }
 
 }
---
+++ src/test/java/org/whiskeysierra/process/internal/ProcessServiceTest.java
@@ -0,0 +1,13 @@
+package org.whiskeysierra.process.internal;
+
+import org.junit.Test;
+
+public final class ProcessServiceTest {
+
+    @Test
+    public void test() {
+
+    }
+
+
+}
