aaee474419663d5d6abe6e2cd31eb5b25da5d878
Willi Schönborn <w.schoenborn@gmail.com>
2013-07-30 01:39:20 +0200

    Updated publish script

--- site/_bin/publish.sh
+++ site/_bin/publish.sh
@@ -5,7 +5,8 @@ set -e
 version=$(git tag | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -n1)
 
 sed -E -i .tmp "s/^version: .*$/version: $version/g" site/index.md
+rm site/index.md
 
 git add site/index.md
-git commit -m "Updated site to version $version"
+git commit -m "Updated site to version $version" --no-verify
 git subtree push --prefix site git@github.com:whiskeysierra/primal.git gh-pages
