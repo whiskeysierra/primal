eab8e4336e2b2ba7700bcab2eacf7b71766ac0a0
Willi Schönborn <w.schoenborn@gmail.com>
2013-07-30 14:26:59 +0200

    Updated site to version 0.2.0

--- _bin/publish.sh
+++ _bin/publish.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-if [ -z $(grep -E "^url:" site/_config.yml) ]; then
+match=$(grep -E "^url:" site/_config.yml)
+if [ -z "$match" ]; then
     echo "Did you miss to set the url key in site/_config.yml?"
     exit 1
 fi
 
 version=$(git tag | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -n1)
 
-sed -E -i .tmp "s/^version: .*$/version: $version/g" site/index.md
+sed -E -i.tmp "s/^version: .*$/version: $version/g" site/index.md
 rm site/index.md.tmp
 
 git add site/index.md
